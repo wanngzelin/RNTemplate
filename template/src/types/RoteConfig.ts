e3d05365ec57d9f5d741908368f4142f48afc02c
王 <wwzzlln@163.com>
2023-05-29 15:59:05 +0800

    fix:更新react-native小版本

--- template/src/types/RoteConfig.ts
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
-import { StackParamList } from "./RouteParamList";
-
-export type RouteConfig = {
-  name: keyof StackParamList,
-  component: React.ComponentType
-  options?: NativeStackNavigationOptions
-}
